638404fd80c012ebfa87fa7bd5c149a629e3e336
whzd <whzdxl@gmail.com>
2023-12-16 17:55:09 +0000

    2023 day9 golang

---
+++ 2023/go/day9/part2/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func allValuesZero(values []int) bool {
+	for _, value := range values {
+		if value != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func calculateDiffList(values []int) []int {
+	diffArray := []int{}
+	for i := 0; i < len(values)-1; i++ {
+		diffArray = append(diffArray, (values[i+1] - values[i]))
+	}
+	return diffArray
+}
+
+func predictPrevValue(values []int) int {
+	if allValuesZero(values) {
+		return 0
+	} else {
+		diff := calculateDiffList(values)
+		return values[0] - predictPrevValue(diff)
+	}
+}
+
+func convertLineToData(line string, oasisData *[][]int) {
+	tmpValues := []int{}
+	for _, value := range strings.Split(line, " ") {
+		num, err := strconv.Atoi(value)
+		if err != nil {
+			fmt.Printf("Unable to convert data num to int! Error: %v", err)
+		}
+		tmpValues = append(tmpValues, num)
+	}
+	*oasisData = append(*oasisData, tmpValues)
+}
+
+func main() {
+
+	file, err := os.Open("./input")
+	if err != nil {
+		log.Fatalf("Unable to open the file: %v\n", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	oasisData := [][]int{}
+
+	for scanner.Scan() {
+		convertLineToData(scanner.Text(), &oasisData)
+	}
+
+	total := 0
+	for _, list := range oasisData {
+		total += predictPrevValue(list)
+	}
+	fmt.Println(total)
+
+}
